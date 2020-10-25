9836c61a51cc4d381ac4c10fc5b3161496c4bbf0
jsiu <jonsiu826@gmail.com>
2020-10-25 11:35:43 -0700

    finished frontend

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-
-app.get("/", (req, res) => res.send("Hellow World"));
-
 const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const passport = require("passport");
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
-const port = process.env.PORT || 5000;
+app.get("/", (req, res) => res.send("Hello World"));
 
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dev:ENNZ3WBwl4lZ2dBJ@cluster0.3nrrm.mongodb.net/MERNdb?retryWrites=true&w=majority",
+  secretOrKey: "secret123"
+};
